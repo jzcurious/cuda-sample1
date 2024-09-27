de9f277e0155fe54060661415415d9e881d14387
jzcurious <paranoid.hpc.io@gmail.com>
2024-09-27 12:40:51 +0700

    fix colab

--- cuda-sample1-lib/include/dotprod.cuh
+++ cuda-sample1-lib/include/dotprod.cuh
@@ -14,10 +14,14 @@
 
 namespace detail {
 
-template <ItemKind T>
-__global__ void dot_naive_kernel(const T* a, const T* b, T* c, size_t len) {
-  size_t i = blockIdx.x * blockDim.x + threadIdx.x;
-  if (i < len) atomicAdd(c, a[i] * b[i]);
+template <size_t persist_coeff, ItemKind T>
+__global__ void dot_persistent_kernel(const T* a, const T* b, T* c, size_t len) {
+  size_t from = persist_coeff * (blockIdx.x * blockDim.x + threadIdx.x);
+  size_t to = from + persist_coeff;
+
+  T acc = 0;
+  for (size_t i = from; i < to and i < len; ++i) acc += a[i] * b[i];
+  atomicAdd(c, acc);
 }
 
 template <ItemKind T>
@@ -31,22 +35,23 @@ template <ItemKind T>
 Scal<T> dot_views(const VecView<T>& vec_a, const VecView<T>& vec_b) {
 #ifdef DEBUG
   if (vec_a.loc() != vec_b.loc())
-    throw std::invalid_argument("the different location of the arguments is not allowed");
+    throw std::invalid_argument("the different location of the arguments is not
+    allowed");
 #endif
 
   size_t len = std::min(vec_a.len(), vec_b.len());
 
   if (vec_a.is_on_host()) {
-    Scal<T> product(0, Loc::Host);
-    detail::dot_host(vec_a.data(), vec_b.data(), product.data(), len);
-    return product;
+      Scal<T> product(0, Loc::Host);
+      detail::dot_host(vec_a.data(), vec_b.data(), product.data(), len);
+      return product;
   }
 
-  size_t grid_size = eval_grid_size(len);
+  size_t grid_size = eval_grid_size_persist(len);
   Scal<T> product(0, Loc::Device);
 
-  detail::dot_naive_kernel<<<grid_size, block_size>>>(
-      vec_a.data(), vec_b.data(), product.data(), len);
+  detail::dot_persistent_kernel<persist_coeff>
+      <<<grid_size, block_size>>>(vec_a.data(), vec_b.data(), product.data(), len);
   return product;
 }
 
